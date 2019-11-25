b319bf08a4e4d067ad0a7e87e93871ea6e30fe5a
Edward Kim <edward@cyberbird.co>
2019-11-25 12:15:13 -0600

    Adding configuration for airbnb based linter and prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WikiRoulette from './WikiRoulette';
 import registerServiceWorker from './registerServiceWorker';
+import WikiRoulette from './WikiRoulette';
 import './scss/index.scss';
 
 ReactDOM.render(<WikiRoulette />, document.getElementById('root'));
