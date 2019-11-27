adc29bd2d41efcdc5cf66d77cf2c1d99f73c9c4e
Edward Kim <edward@cyberbird.co>
2019-11-27 15:40:10 -0600

    Running tests in pre-commit hook

--- src/components/WikiRoulette.test.js
+++ src/components/WikiRoulette.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WikiRoulette from '../components/WikiRoulette';
+import WikiRoulette from './WikiRoulette';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
